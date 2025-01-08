acf1b02030ac6d5e6e03c2afc5c034469504194a
lipaa1 <philippesgama@gmail.com>
2025-01-08 20:38:38 -0300

    Backend project created

---
+++ dscatalogv2/src/main/java/com/phgama/dscatalog/Dscatalogv2Application.java
@@ -0,0 +1,13 @@
+package com.phgama.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalogv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalogv2Application.class, args);
+	}
+
+}
