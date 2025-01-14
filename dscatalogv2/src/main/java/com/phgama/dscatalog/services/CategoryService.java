db46e08fadcf803b022ece5988309513ecec9bf8
lipaa1 <philippesgama@gmail.com>
2025-01-13 21:32:19 -0300

    Category DTO created

--- dscatalogv2/src/main/java/com/phgama/dscatalog/services/CategoryService.java
+++ dscatalogv2/src/main/java/com/phgama/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.phgama.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.phgama.dscatalog.dto.CategoryDTO;
 import com.phgama.dscatalog.entities.Category;
 import com.phgama.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +19,16 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll(); 
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/* metodo nao usual
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		*/
 	}
 	
 	
